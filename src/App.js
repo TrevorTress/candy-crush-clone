1dde8e319cb7d66c15e1527fe0859e1fc17a5333
Trevor Tress <trevor.tress@outlook.com>
2023-01-26 14:30:06 -0500

    initial board set-up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import react dependencies
+import { useState, useEffect } from 'react';
+// set board size and color choice array
+const width = 8;
+const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow'];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	// board state is managed by useState amd the set function
+	const [boardState, setBoardState] = useState([]);
+
+	//function that generates initial board layout
+	const createBoard = () => {
+		//initialize empty arrangement array
+		const randomColorArrangement = [];
+
+		// for 0-64, pick a random color from the choice array, push it into arrangement array
+		for (let i = 0; i < width * width; i++) {
+			const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)];
+			randomColorArrangement.push(randomColor);
+		}
+		// set the board state with the arrangement obtained above
+		setBoardState(randomColorArrangement);
+	};
+
+	// run board set-up on initial render
+	useEffect(() => {
+		createBoard();
+	}, []);
+
+	return (
+		<div className="app">
+			<div></div>
+		</div>
+	);
+};
 
 export default App;
